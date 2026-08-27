944fedb1d7b234d87bf145dae803f26b7586616a
Ricks86 <ju.fernandeza@duocuc.cl>
2026-08-27 10:26:16 -0400

    Revert "Desafio Salmonera"
    This reverts commit c10eb8f1c8f86a762b3c20a82da8b9d23d895611.

--- src/main/kotlin/CentroCulivo.kt
+++
@@ -1,9 +0,0 @@
-package org.example
-
-data class CentroCulivo(
-    val id: Int,
-    val nombre: String,
-    val ubicacion: String,
-    var produccionToneladas: Double,
-    var Encargado: String? = null
-)
